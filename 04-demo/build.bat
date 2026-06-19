8e3d40ad5816a5fd6574601f784fc4f6d125326f
Ivan Avdeev <me@provod.works>
2026-06-19 16:31:17 -0400

    04: add the full intro with shader

---
+++ 04-demo/build.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+call ..\setupenv.bat || exit /b 1
+
+..\sointu-compile.exe -o music -e h,asm -arch=386 music.yml || exit /b 3
+
+REM fixup alignment
+powershell -Command "(gc music.asm) -replace 'align=256', 'align=64' | Out-File -encoding ASCII music.asm" || exit /b 4
+..\nasm.exe -fwin32 -o music.obj music.asm || exit /b 5
+
+cl /c /GS- intro.c || exit /b 2
+
+..\crinkler.exe ^
+	intro.obj music.obj ^
+	kernel32.lib user32.lib opengl32.lib gdi32.lib winmm.lib ^
+	/subsystem:windows /OUT:intro.exe /CRINKLER
